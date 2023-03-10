3b5dd7f115b5e9779b3efcdb6533d25d09ec7912
lizzavolk <lizavolkova080207@gmail.com>
2023-03-10 16:57:30 +0300

    Fourth commit

---
+++ muscular-system.js
@@ -0,0 +1,24 @@
+"use strict";
+window.onload = (event) => {
+ document.querySelector('.body-container .hover-block.right-arm').onclick = function(){
+  document.querySelector('.inf-container .ollo').style.display =  document.querySelector('.inf-container .ollo').style.display == 'none' ? '' : 'none';
+   document.querySelector('.inf-container .hands').style.display =  document.querySelector('.inf-container .hands').style.display == '' ? 'block' : '' ;
+ };
+ document.querySelector('.body-container .hover-block.left-arm').onclick = function(){
+   document.querySelector('.inf-container .ollo').style.display =  document.querySelector('.inf-container .ollo').style.display == 'none' ? '' : 'none';
+   document.querySelector('.inf-container .hands').style.display =  document.querySelector('.inf-container .hands').style.display == '' ? 'block' : '' ;
+ };
+ document.querySelector('.body-container .hover-block.legs').onclick = function(){
+   document.querySelector('.inf-container .ollo').style.display =  document.querySelector('.inf-container .ollo').style.display == 'none' ? '' : 'none';
+   document.querySelector('.inf-container .legs').style.display =  document.querySelector('.inf-container .legs').style.display == '' ? 'block' : '' ;
+ };
+ document.querySelector('.body-container .hover-block.headd').onclick = function(){
+  document.querySelector('.inf-container .ollo').style.display =  document.querySelector('.inf-container .ollo').style.display == 'none' ? '' : 'none';
+   document.querySelector('.inf-container .headd').style.display =  document.querySelector('.inf-container .headd').style.display == '' ? 'block' : '' ;
+ };
+ document.querySelector('.body-container .hover-block.gryd').onclick = function(){
+   document.querySelector('.inf-container .ollo').style.display =  document.querySelector('.inf-container .ollo').style.display == 'none' ? '' : 'none';
+   document.querySelector('.inf-container .gryd').style.display =  document.querySelector('.inf-container .gryd').style.display == '' ? 'block' : '' ;
+ };
+ console.log("page is fully loaded");
+};
